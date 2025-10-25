2fa57c9822fd69d0a2990ec7f9fd4e18c3ac6c32
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2025-10-25 15:40:55 +0600

    Screen : HomeScreen, Feat : Current Weight -> Drop Down, Status : Done

--- lib/controllers/home_screen_controller.dart
+++ lib/controllers/home_screen_controller.dart
@@ -1,7 +1,11 @@
 import 'package:bloodfit/constants/app_enums.dart';
+import 'package:bloodfit/controllers/weight_picker_widget_controller.dart';
+import 'package:bloodfit/gen/colors.gen.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeScreenController extends GetxController {
+  ///Section : ----------------////Selectable Meal Calendar for Meal Plan///----------------------
   RxInt mealCalanderSelectableDays = 3.obs;
   List<WeekDayEnum> weekDayList = WeekDayEnum.values;
   RxList<WeekDayEnum> selectedDaysList = <WeekDayEnum>[].obs; // Keep as RxList
@@ -16,6 +20,23 @@ class HomeScreenController extends GetxController {
         Get.snackbar(
           'Limit Reached',
           'You can only select ${mealCalanderSelectableDays.value} days',
+          backgroundColor: AppColors.cb20000.withOpacity(
+            0.2,
+          ), // translucent “glass” white
+          colorText: Colors.white, // readable on translucent bg
+          snackPosition: SnackPosition.TOP,
+          borderRadius: 16,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          borderWidth: 1,
+          borderColor: Colors.white.withOpacity(0.3),
+          boxShadows: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.1),
+              blurRadius: 12,
+              offset: const Offset(0, 6),
+            ),
+          ],
+          icon: const Icon(Icons.info_outline, color: AppColors.cb20000),
         );
       }
     }
@@ -24,5 +45,21 @@ class HomeScreenController extends GetxController {
 
   bool isDaySelected(WeekDayEnum day) {
     return selectedDaysList.contains(day);
+  }
+
+  ///Section : ---------------------///Update Your Current Weight drop down///-------------------
+  var selectedWeightUnit = 'Kg'.obs;
+  TextEditingController weightController = TextEditingController();
+
+  ///Feat : -> Set Value of Weight UnitType
+  void setSelectedWeightUnit({required String unit}) {
+    selectedWeightUnit.value = unit;
+  }
+
+  ///Feat : -> Close the weight controller
+  @override
+  void onClose() {
+    weightController.dispose();
+    super.onClose();
   }
 }
---
+++ lib/custom_widgets/current_weight_update_widget.dart
@@ -0,0 +1,105 @@
+import 'package:bloodfit/controllers/home_screen_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../../../../constants/text_font_style.dart';
+import '../../../../gen/colors.gen.dart';
+import '../../../../helper/ui_helpers.dart';
+
+class CurrentWeightUpdateWidget extends StatelessWidget {
+  CurrentWeightUpdateWidget({super.key});
+
+  final HomeScreenController homeScreenController =
+      Get.find<HomeScreenController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Update Your Current Weight",
+          style: TextFontStyle.headline20w500cfefefeStylePoppins,
+        ),
+        UIHelper.verticalSpace(8.h),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 15.w),
+          decoration: BoxDecoration(
+            color: AppColors.c262626,
+            borderRadius: BorderRadius.circular(16.r),
+          ),
+          child: TextFormField(
+            controller: homeScreenController.weightController,
+            style: TextFontStyle.headline14w700cfefefeStylePoppins,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              hintText: "Enter Your Weight",
+              hintStyle: TextFontStyle.headline12w500c999999StylePoppins,
+
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.cb20000),
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.cb20000),
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.cb20000),
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+
+              disabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.cb20000),
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+              suffixIcon: Container(
+                margin: EdgeInsets.symmetric(vertical: 12.h, horizontal: 15.w),
+                padding: EdgeInsets.symmetric(vertical: 3.h, horizontal: 10.w),
+                decoration: BoxDecoration(
+                  color: AppColors.c363636,
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+
+                child: Obx(() {
+                  return DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      padding: EdgeInsets.zero,
+
+                      isDense: true,
+                      dropdownColor: AppColors.c262626,
+                      value: homeScreenController
+                          .selectedWeightUnit
+                          .value, // default value
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: AppColors.cfefefe,
+                      ),
+                      items: ['Kg', 'lb'].map((unit) {
+                        return DropdownMenuItem<String>(
+                          value: unit,
+                          child: Text(
+                            unit,
+                            style:
+                                TextFontStyle.headline12w500cfefefeStylePoppins,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        homeScreenController.setSelectedWeightUnit(
+                          unit: value ?? "",
+                        );
+                      },
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
