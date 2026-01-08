03f4bcadb4b6ae3701c9a72dfa5dfdb3a43d581d
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2026-01-08 10:06:14 +0600

    Screen : Forgot Password, Section : Imported the Repository to Controller and Bindings, Status : Done

--- lib/features/auth/forgot_password/binding/forgot_password_binding.dart
+++ lib/features/auth/forgot_password/binding/forgot_password_binding.dart
@@ -1,9 +1,16 @@
 import 'package:bloodfit/features/auth/forgot_password/data/controller/forgot_password_controller.dart';
 import 'package:get/get.dart';
 
+import '../data/repository/forgot_password_repository.dart';
+
 class ForgotPasswordBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<ForgotPasswordController>(() => ForgotPasswordController());
+    Get.lazyPut<ForgotPasswordRepository>(
+      () => ForgotPasswordRepository(Get.find()),
+    );
+    Get.lazyPut<ForgotPasswordController>(
+      () => ForgotPasswordController(Get.find()),
+    );
   }
 }
