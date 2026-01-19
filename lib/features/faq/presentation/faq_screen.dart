2bfe4dd43076a3fdb4e2bf27b917512ed0ebea04
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2026-01-19 15:32:24 +0600

    Screen : Faq Screen, Section : Api Integration, Status : Completed

--- lib/features/faq/presentation/faq_screen.dart
+++ lib/features/faq/presentation/faq_screen.dart
@@ -1,5 +1,6 @@
 // 📁 faq_screen.dart
 import 'package:bloodfit/constants/app_list.dart';
+import 'package:bloodfit/custom_widgets/custom_shimmer_effect.dart';
 import 'package:bloodfit/features/faq/presentation/widgets/faq_expansaion_tile.dart';
 import 'package:bloodfit/helper/ui_helpers.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,9 @@ class FaqScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final FaqScreenController controller = Get.find<FaqScreenController>();
-
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      controller.getFaqScreenApi();
+    });
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackgroundColor,
       appBar: AppBar(
@@ -33,24 +36,39 @@ class FaqScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(UIHelper.kDefaulutPadding()),
-          child: ListView.separated(
-            itemCount: AppList.faqList.length,
-            separatorBuilder: (context, index) => UIHelper.verticalSpace(10.h),
-            itemBuilder: (context, index) {
-              final data = AppList.faqList[index];
+          child: Obx(() {
+            return controller.isLoading.value
+                ? ListView.separated(
+                    itemCount: 10,
+                    separatorBuilder: (context, index) =>
+                        UIHelper.verticalSpace(10.h),
 
-              return Obx(() {
-                final isVisible = controller.expandedIndex.value == index;
+                    itemBuilder: (context, index) {
+                      return CustomShimmerEffect(height: 20.h, width: 1.sw);
+                    },
+                  )
+                : ListView.separated(
+                    // itemCount: AppList.faqList.length,
+                    itemCount: controller.faqList.length,
+                    separatorBuilder: (context, index) =>
+                        UIHelper.verticalSpace(10.h),
+                    itemBuilder: (context, index) {
+                      final data = controller.faqList[index];
 
-                return FaqExpansionTile(
-                  question: data.question,
-                  ans: data.ans,
-                  isAnsVisible: isVisible,
-                  onTap: () => controller.toggleExpand(index),
-                );
-              });
-            },
-          ),
+                      return Obx(() {
+                        final isVisible =
+                            controller.expandedIndex.value == index;
+
+                        return FaqExpansionTile(
+                          question: data.question ?? 'Question Not Found!',
+                          ans: data.answer ?? 'Answer Not Found!',
+                          isAnsVisible: isVisible,
+                          onTap: () => controller.toggleExpand(index),
+                        );
+                      });
+                    },
+                  );
+          }),
         ),
       ),
     );
