29c76d527558c27a1a0f9e8bdccc436c55067eef
Imtiaz Chowdhury <work.imtiazchowdhury@gmail.com>
2026-03-05 13:37:18 +0600

    Screen : Choose From Our Suggested Meals, Section : Meal Selection update showing widget, status : On-Going

--- lib/features/choose_from_our_suggested_meals/presentation/widgets/meal_plan_selection_tracker.dart
+++ lib/features/choose_from_our_suggested_meals/presentation/widgets/meal_plan_selection_tracker.dart
@@ -94,6 +94,8 @@ class MealPlanSelectionTracker extends StatelessWidget {
                 buttonWidth: 100.w,
                 borderRadius: 8.r,
                 isDisabled: !isMealPlanComplete,
+
+                buttonColor: Colors.amber,
               );
             }),
           ],
